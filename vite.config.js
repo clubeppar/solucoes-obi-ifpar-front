53434cf1afe2ff971fcd70a626cdb94ead15d153
brasilicioh <brasiliciohcr@gmail.com>
2026-02-27 11:04:56 -0300

    chore: init tailwind in vite project

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/solucoes-obi-ifpar/',
+  base: "/solucoes-obi-ifpar/",
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tailwindcss(),
   ],
-})
+});
