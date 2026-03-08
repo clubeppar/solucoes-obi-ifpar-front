018b60c335c86ff3245727f0ab4bf080a5fc2d9f
Kaio Henrique <kaio.lopes@escolar.ifrn.edu.br>
2026-03-08 19:49:33 -0300

    feat: New design with Shadow, color change, added components to index.css

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,26 +1,22 @@
 //LEMBRAR: adicionar as imagens nos assets depois, puxar imagens da internet é uma má ideia.
 function Topbar(){
   return(
-    <header className="flex p-3 w-screen justify-between
-    border-b-3 border-blue-600 border-solid
-    ">
+    <header className="flex w-screen justify-between relative z-10 shadow-xl/60">
+      
+      
       <img          
-      className="size-12 invert" 
+      className="size-12 invert cursor-pointer ms-4" 
       src="https://cdn-icons-png.flaticon.com/512/711/711284.png"      
       alt="" />
 
-      <div className="flex align-center">
-        <button className="text-white hover:text-blue-500 font-bold px-3 py-1 m-1">
+      <div className="flex align-center me-4">
+        <button className="list-item">
           Inicio
         </button>
-        <button className="text-white hover:text-blue-500 font-bold px-3 py-1 m-1">
+        <button className="list-item">
           Questões
         </button>
-        <button className="bg-blue-500 text-white font-bold px-3 py-1 m-1 rounded
-         hidden sm:flex items-center gap-2
-         border-b-4 border-blue-600
-         hover:bg-blue-400
-         hover:border-b-0"
+        <button className="btn-info-3d hidden sm:flex items-center gap-2 cursor-pointer"
          onClick={()=> window.location.href= "https://github.com/G-Aleixo/solucoes-obi-ifpar"}
         >
           <img className="size-6" src="https://cdn-icons-png.flaticon.com/256/25/25231.png" alt="" />
