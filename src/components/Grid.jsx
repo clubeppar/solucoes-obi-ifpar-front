3f518f01527bed399f8369fb35b0b0c4c13ee069
Kaio Henrique <kaio.lopes@escolar.ifrn.edu.br>
2026-03-30 20:06:06 -0300

    feat: spinner fully added and working

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -18,9 +18,8 @@ export default function Grid() {
 				
 				<MainPage />
 
-				{/* <Loading /> */}
 				<Footer />
 			</div>
-			</>
+		</>
 	);
 }
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,22 +1,30 @@
 import { useState, useEffect } from "react";
 
 export function Loading(){
-  const [secondsLeft, setSecondsLeft] = useState(3);
-  const [text, setText] = useState("Carregando.")
+  const [text, setText] = useState("Carregando")
+  const [count, setCount] = useState(1)
 
   useEffect(() => {
-    setTimeout(() => {setText(text += ".")}, 1000)
+    setTimeout(() => {setCount(count + 1); 
+      if(count%4==0){
+        setText("Carregando")
+      } else{
+        setText(text + ".")
+      }
+    }, 500)
   })
 
-
-  
-  
   return(
-    <div className="z-999 flex flex-col justify-center items-center fixed w-screen h-screen bg-blue-950/25">
+    <div className="z-999 flex flex-col justify-center items-center fixed w-screen h-screen bg-gray-950/50">
       <img src="https://www.svgrepo.com/show/315795/spinner.svg" alt="" 
       className="animate-spin size-20 invert"
       />
       <p className="text-white font-semibold text-xl">{text}</p>
+      <div className="h-10 flex items-center">
+        {count > 24 && (
+          <p className="font-semibold text-white text-2xl pt-3">Algo deu errado, aguarde um pouco mais</p>
+        )}
+      </div>
     </div>
   )
 } export default Loading;
