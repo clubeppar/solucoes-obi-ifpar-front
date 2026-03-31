bba2f358a84f817a6aa2129b5bde4c9a56da21d6
thifre09 <128859182+thifre09@users.noreply.github.com>
2026-03-31 15:36:29 -0300

    feat: created MainPageInput and MainPageResults components

---
+++ src/components/subcomponents/MainPageResults.jsx
@@ -0,0 +1,27 @@
+import Teste from "../subcomponents/Teste";
+
+export default function MainPageResults({ testes }) {
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <h1 className="w-19/20 m-3 text-2xl ">Resultado:</h1>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="w-19/20 flex justify-between">
+          {testes.map((t) => (
+            <Teste
+              key={t.nTeste}
+              nTeste={t.nTeste}
+              status={t.status}
+              tempoTeste={t.tempoTeste}
+              menorTempo={t.menorTempo}
+              usoMemoria={t.usoMemoria}
+              tipoFalha={t.tipoFalha}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
