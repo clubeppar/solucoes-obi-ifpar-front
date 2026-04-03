97ab280806c4535c2bcd4deb2a0a9b4980ea64d0
brasilicioh <brasiliciohcr@gmail.com>
2026-04-03 09:03:21 -0300

    fix: use react-icons; fixing gramatical error and use handle functions

--- src/components/global_components/Topbar.jsx
+++ src/components/global_components/Topbar.jsx
@@ -1,46 +1,39 @@
-//LEMBRAR: adicionar as imagens nos assets depois, puxar imagens da internet é uma má ideia.
+import { FiSun } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa";
+
 export default function Topbar({ collapsed }) {
+  const handleThemeToggle = () => {
+    console.log("implementar sun/dark mode");
+  };
+
+  const handleGithubClick = () => {
+    window.location.href = "https://github.com/g-aleixo/solucoes-obi-ifpar/";
+  };
+
   return (
     <header className="grid grid-cols-3 items-center px-3 z-10 w-full">
-      {!collapsed && (
+      {!collapsed ? (
         <img
           className="size-12 invert cursor-pointer ms-4"
           src="https://cdn-icons-png.flaticon.com/512/711/711284.png"
-          alt=""
+          alt="Logo do Clube de Programação do IFPAR"
         />
+      ) : (
+        <div></div>
       )}
 
-      {collapsed && <div></div>}
-
       <div className="flex justify-center">
-        <button className="list-item">Inicio</button>
+        <button className="list-item">Início</button>
         <button className="list-item">Questões</button>
         <button className="list-item">Sobre Nós</button>
       </div>
 
       <div className="flex justify-end items-center gap-2">
-        <button className="h-9 items-center cursor-pointer">
-          <img
-            src="https://static.thenounproject.com/png/4808961-200.png"
-            alt=""
-            className="invert size-8"
-          />
+        <button className="cursor-pointer" onClick={handleThemeToggle}>
+          <FiSun className="size-8 text-white" />
         </button>
-        <button>
-          <img
-            src="https://cdn-icons-png.flaticon.com/256/25/25231.png"
-            alt=""
-            className="size-6 invert"
-          />
-        </button>
-        <button
-          className="btn-info-3d h-9 hidden sm:flex items-center gap-2 cursor-pointer"
-          onClick={() =>
-            (window.location.href =
-              "https://github.com/G-Aleixo/solucoes-obi-ifpar")
-          }
-        >
-          Github
+        <button className="cursor-pointer" onClick={handleGithubClick}>
+          <FaGithub className="size-8 text-white" />
         </button>
       </div>
     </header>
