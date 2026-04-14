cbab277ff1210ed49c3a5bad0a62b7060d148d0d
Alanderson-LS <delimasoaresalanderson@gmail.com>
2026-04-14 10:12:47 -0300

    feat: started working on credits

--- src/components/pages/Credits.jsx
+++ src/components/pages/Credits.jsx
@@ -1,7 +1,22 @@
+import Topbar from "../global_components/Topbar";
+
 export default function Credits() {
   return (
     <>
-      <p>teste créditos</p>
+      <div className="grid grid-cols-4 grid-rows-[auto_6fr_auto] min-h-screen gap-0">
+        <div className="col-span-full bg-gray-950 flex items-center h-16">
+          <Topbar colapsed={false} />
+        </div>
+
+        <div className="col-span-full bg-slate-900 light:bg-white flex flex-col items-center justify-start text-white">
+          <div className="text-5xl font-bold text-center mt-16 mb-16">
+            Contribuições
+          </div>
+          <div className="grid grid-cols-4">
+            
+          </div>
+        </div>
+      </div>
     </>
   );
 }
