5021af1f5bb2605f115018ecec44f2f80a2c680c
brasilicioh <brasiliciohcr@gmail.com>
2026-04-15 18:47:09 -0300

    refactor: update contributors const and fix credits's footer style. Button navigate to init now

--- src/components/pages/Credits.jsx
+++ src/components/pages/Credits.jsx
@@ -1,9 +1,14 @@
-import Topbar from "../global_components/Topbar";
+import { useNavigate } from "react-router-dom";
 
-import ContributorCard from "../subcomponents/credits/contributorcard";
-import { contributors } from "../subcomponents/credits/contributors";
+import Topbar from "@/global_components/Topbar";
+import Footer from "@/global_components/Footer";
+
+import ContributorCard from "@/subcomponents/credits/contributorcard";
+import { CONTRIBUTORS } from "@/subcomponents/credits/contributors";
 
 export default function Credits() {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="grid grid-cols-4 grid-rows-[auto_6fr_auto] min-h-screen gap-0">
@@ -16,28 +21,32 @@ export default function Credits() {
             Contribuições
           </div>
           <div className="grid max-w-6xl grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full justify-content:center">
-            {contributors.map((person, index) => (
-              <ContributorCard className="lm-[26px]"
-              key={index}
-              name={person.name}
-              role={person.role}
-              image={person.image}
+            {CONTRIBUTORS.map((person, index) => (
+              <ContributorCard
+                key={index}
+                name={person.name}
+                role={person.role}
+                image={person.image}
               />
             ))}
           </div>
-          <div className="fixed bottom-0 bg-slate-900 w-full flex justify-center flex-col items-center light:bg-white"> 
-            <div className="w-fit mt-[8px] text-center text-[16px]/[24px] font-normal display-inline-block  border-2 border-gray-700 rounded-[20px]  light:text-black">
+          <div className="bottom-0 bg-slate-900 w-full flex justify-center flex-col items-center light:bg-white">
+            {/* tentar fazer mais parecido com o figma */}
+            <div className="w-fit mt-2 text-center text-[16px]/[24px] font-normal display-inline-block  border-2 border-gray-700 rounded-[20px]  light:text-black">
               Feito por alunos, para alunos
             </div>
-            <button className="mt-[16px] mb-[16px] bg-blue-600 pr-[40px] pl-[40px] pt-[10px] pb-[10px] rounded-[8px] text-[14px]/[20px] font-medium">
+            {/* mudar cor do botão quando hover */}
+            <button
+              onClick={() => navigate("/")}
+              className="mt-4 mb-4 bg-blue-600 pr-10 pl-10 pt-2.5 pb-2.5 rounded-lg text-[14px]/[20px] font-medium cursor-pointer transition hover:bg-gray-400"
+            >
               Voltar para o inicio
             </button>
-            <div className=" mb-[12px] text-gray-400 text-[14px]/[20px] font-medium">
-              Clube de Programação IFPAR
-            </div>
           </div>
         </div>
       </div>
+
+      <Footer />
     </>
   );
 }
