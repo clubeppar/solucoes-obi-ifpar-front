6a20471c46423ebf729353a4a85dfc73dbbe8bb9
Alanderson-LS <delimasoaresalanderson@gmail.com>
2026-04-15 11:28:58 -0300

    feat: add to credits the cards of each person

---
+++ src/components/subcomponents/credits/Contributors.jsx
@@ -0,0 +1,80 @@
+import kaioimg from "../../../../images/kaio.png"
+import brasilicioimg from "../../../../images/brasilicio.png"
+import guilhermeimg from "../../../../images/guilherme.png"
+import gabrielimg from "../../../../images/gabriel.png"
+import douglasimg from "../../../../images/douglas.png"
+import leonardoimg from "../../../../images/leonardo.png"
+import gustavoimg from "../../../../images/gustavo.png"
+import brunoimg from "../../../../images/bruno.png"
+import alandersonimg from "../../../../images/alanderson.png"
+import ritaimg from "../../../../images/rita.png"
+import thiagoimg from "../../../../images/thiago.png"
+
+export const contributors = [
+    {
+        id: 1,
+        name: "Kaio Henrique",
+        role: "Frontend | Coordenador",
+        image: kaioimg
+    },
+    {
+        id: 2,
+        name: "Brasilicio Campos",
+        role: "Fullstack | Coordenador",
+        image: brasilicioimg
+    },
+    {
+        id: 3,
+        name: "Guilherme Aleixo",
+        role: "Backend | Coordenador",
+        image: guilhermeimg
+    },
+    {
+        id: 4,
+        name: "Gabriel Nascimento",
+        role: "Design",
+        image: gabrielimg
+    },
+    {
+        id: 5,
+        name: "Douglas Ryan",
+        role: "Frontend",
+        image: douglasimg
+    },
+    {
+        id: 6,
+        name: "Bruno Gustavo",
+        role: "Backend",
+        image: brunoimg
+    },
+    {
+        id: 7,
+        name: "Leonardo Kauffman",
+        role: "Backend",
+        image: leonardoimg
+    },
+    {
+        id: 8,
+        name: "Gustavo Andrey",
+        role: "Frontend | Design",
+        image: gustavoimg
+    },
+    {
+        id: 9,
+        name: "Rita Cássia",
+        role: "Fullstack",
+        image: ritaimg
+    },
+    {
+        id: 10,
+        name: "Thiago Freitas",
+        role: "Frontend",
+        image: thiagoimg
+    },
+    {
+        id: 11,
+        name: "Alanderson Lima",
+        role: "Frontend",
+        image: alandersonimg
+    },
+]
