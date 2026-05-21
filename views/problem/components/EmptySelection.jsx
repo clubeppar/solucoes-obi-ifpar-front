889921d7d11f4b3ead26b40a955d008892d7e310
thifre09 <128859182+thifre09@users.noreply.github.com>
2026-05-21 11:24:01 -0300

    feat: updated EmptySelection component

--- views/problem/components/EmptySelection.jsx
+++ views/problem/components/EmptySelection.jsx
@@ -1,3 +1,10 @@
+import { PiSmileySad } from "react-icons/pi";
+
 export function EmptySelection() {
-  return <p>Nenhum problema selecionado ainda</p>;
+  return (
+    <div className="flex flex-col h-full w-full items-center justify-center">
+      <PiSmileySad className="size-16 text-gray-500"/>
+      <p className="text-2xl text-gray-500">Nenhum problema selecionado ainda</p>
+    </div>
+  );
 }
