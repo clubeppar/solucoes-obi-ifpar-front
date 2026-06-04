fa6330ef19b378ec9207460240b15003a9dfa48e
brasilicioh <brasiliciohcr@gmail.com>
2026-06-04 14:19:10 -0300

    feat: include notification's mapping logic and global provider resource

--- shared/components/Notification.jsx
+++ shared/components/Notification.jsx
@@ -1,37 +1,30 @@
-import { FaCheck } from "react-icons/fa";
-import { MdErrorOutline } from "react-icons/md";
-import { IoWarningOutline } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 
-const statusType = Object.freeze({
-    "success": {
-        icon: <FaCheck />,
-        color: 'green-400',
-    },
-    "error": {
-        icon: <MdErrorOutline />,
-        color: 'red-500',
-    },
-    "warning": {
-        icon: <IoWarningOutline />,
-        color: 'yellow-400',
-    },
-});
+import { useNotification } from "@hooks";
 
-export function Notification({ type, message }) {
-    return (
-        <div className={`justify-between gap-2 rounded-lg p-4 text-sm font-medium bg-gray-900 w-fit min-w-75 flex`}>
-            <div className="flex items-center gap-2">
-                <div className={`border rounded-md bg-gray-800 p-1 border-${statusType[type].color}`}>
-                    <span className={`text-${statusType[type].color}`}>
-                        {statusType[type].icon}
-                    </span>
-                </div>
-                {message}
-            </div>
-            <button className="ml-auto text-gray-400 hover:text-gray-500">
-                <IoMdClose  className="size-5"/>
-            </button>
+export function Notification() {
+  const { notification, hideNotification } = useNotification();
+
+  if (!notification) return null;
+
+  return (
+    <div
+      className={`justify-between gap-2 rounded-lg p-4 text-sm font-medium bg-gray-900 w-fit min-w-75 flex`}
+    >
+      <div className="flex items-center gap-2">
+        <div
+          className={`border rounded-md bg-gray-800 p-1 ${notification.borderClass}`}
+        >
+          <span className={notification.textClass}>{notification.icon}</span>
         </div>
-    );
+        {notification.message}
+      </div>
+      <button
+        onClick={hideNotification}
+        className="ml-auto text-gray-400 hover:text-gray-500"
+      >
+        <IoMdClose className="size-5" />
+      </button>
+    </div>
+  );
 }
