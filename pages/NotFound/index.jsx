a5e01992de647347f0e6942ac769d5d897353551
Júlio César <juliocesaroalves679@gmail.com>
2026-06-08 20:48:19 -0300

    style: Adjustments to the responsiveness of Not-Found page

--- pages/NotFound/index.jsx
+++ pages/NotFound/index.jsx
@@ -5,12 +5,12 @@ import capivara_NotFound from "assets/public/capivara_NotFound.png";
 
 export function NotFound() {
   return (
-    <div className="justify-center content-center bg-[#030712]">
+    <div className="min-h-screen w-full justify-center content-center bg-[#030712]">
       <div className="landingpage-topbar-slot">
         <Topbar collapsed={false} />
       </div>
-      <div className="w-full mt-5 min-w-75 p-5 text-white flex flex-col sm:flex-row justify-center content-center">
-        <div className="h-60 sm:h-158 text-center sm:text-left flex flex-col justify-center content-center gap-2">
+      <div className="min-w-full min-w-75 p-5 text-white flex flex-col mr-auto ml-auto md:flex-row justify-center content-center">
+        <div className="md:h-158 text-center md:text-left flex flex-col justify-center content-center">
           <h1 className="text-[120px] font-black">404</h1>
           <p className="mb-10 text-[20px]">
             <p className="font-black">Ops... Ocorreu um erro.</p>
@@ -25,10 +25,10 @@ export function NotFound() {
             </button>
           </Link>
         </div>
-        <img src={capivara_NotFound} alt="Imagem Not Found" className="w-120" />
+        <img src={capivara_NotFound} alt="Imagem de Not-Found" className="w-120 ml-auto mr-auto md:ml-0 md:mr-0" />
       </div>
 
-      <div className="col-span-full flex w-full">
+      <div className="col-span-full flex w-full h-full">
         <Footer />
       </div>
     </div>
